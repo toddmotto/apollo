9c2223583941ef9d598cef1e9295e5630a49d67c
Matthias Le Brun <mlbli@me.com>
2013-11-18 17:01:41 +0100

    Removed the feature testing from each executed method and made these methods proper to the given environment

--- dist/apollo.js
+++ dist/apollo.js
@@ -5,46 +5,55 @@
  *  by Todd Motto: http://toddmotto.com
  *  Copyright. MIT licensed.
  */
-window.Apollo = (function (window, document, undefined) {
+window.Apollo = (function (window, document) {
 
   'use strict';
 
   var classList = document.documentElement.classList;
 
-  var apollo = {
-    hasClass: function (elem, className) {
-      if (classList) {
-        return elem.classList.contains(className);
-      } else {
-        return new RegExp('(^|\\s)' + className + '(\\s|$)').test(elem.className);
-      }
-    },
-    addClass: function (elem, className) {
-      if (classList) {
-        elem.classList.add(className);
-      } else {
-        if (!apollo.hasClass(elem, className)) {
-          elem.className += (elem.className ? ' ' : '') + className;
-        }
-      }
-    },
-    removeClass: function (elem, className) {
-      if (classList) {
-        elem.classList.remove(className);
-      } else {
-        if (apollo.hasClass(elem, className)) {
-          elem.className = elem.className.replace(new RegExp('(^|\\s)*' + className + '(\\s|$)*', 'g'), '');
-        }
-      }
-    },
-    toggleClass: function (elem, className) {
-      if (classList) {
-        elem.classList.toggle(className);
-      } else {
-        var toggle = apollo.hasClass(elem, className) ? apollo.removeClass : apollo.addClass;
-        toggle(elem, className);
-      }
+  var apollo = {};
+
+  if(classList) {
+
+    apollo.hasClass = function (elem, className) {
+      return elem.classList.contains(className);
+    };
+
+    apollo.addClass = function (elem, className) {
+      elem.classList.add(className);
+    };
+
+    apollo.removeClass = function (elem, className) {
+      elem.classList.remove(className);
+    };
+
+    apollo.toggleClass = function (elem, className) {
+      elem.classList.toggle(className);
+    };
+
+    return apollo; 
+  }
+
+
+  apollo.hasClass = function (elem, className) {
+    return new RegExp('(^|\\s)' + className + '(\\s|$)').test(elem.className);
+  };
+
+  apollo.addClass = function (elem, className) {
+    if (!apollo.hasClass(elem, className)) {
+      elem.className += (elem.className ? ' ' : '') + className;
     }
+  };
+
+  apollo.removeClass = function (elem, className) {
+    if (apollo.hasClass(elem, className)) {
+      elem.className = elem.className.replace(new RegExp('(^|\\s)*' + className + '(\\s|$)*', 'g'), '');
+    }
+  };
+
+  apollo.toggleClass = function (elem, className) {
+    var toggle = apollo.hasClass(elem, className) ? apollo.removeClass : apollo.addClass;
+    toggle(elem, className);
   };
 
   return apollo;
